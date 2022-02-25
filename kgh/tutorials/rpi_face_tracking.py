26e8decda626f5e40d2eab51748fcb38be340dd8
jubil2gh <assodium@naver.com>
2022-02-25 21:25:17 +0900

    22.0225.2123

--- kgh/tutorials/rpi_face_tracking.py
+++ kgh/tutorials/rpi_face_tracking.py
@@ -5,22 +5,26 @@ Created on Sun Nov 14 16:09:19 2021
 @author: natha
 """
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
 import datetime
 import os
 
 cur_dir = os.getcwd() + '\\'
 
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-record = False
+cap = PiCamera()
+rawCapture = PiRGBArray(cap)
+time.sleep(0.1)
 
 face_cascade = cv2.CascadeClassifier('./imported_model/haarcascade_frontalface_default.xml')
 # face_cascade = cv2.CascadeClassifier('./imported_model/haarcascade_eye.xml')
 # face_cascade = cv2.CascadeClassifier('./imported_model/haarcascade_lefteye_2splits.xml')
 
 while True:
-    ret, img = cap.read()
+    cap.capture(rawCapture, format='bgr')
+    img = rawCapture.array
     
     if not ret:
         break
@@ -38,21 +42,6 @@ while True:
     
     if key == 27:
         break
-    elif key == ord('c'):
-        print('Capture')
-        cv2.imwrite(cur_dir + str(now) + ".png", img)
-    elif key == ord('s'):
-        print("Video Rec Start")
-        record = True
-        video = cv2.VideoWriter(cur_dir + str(now) + ".avi", fourcc, 20.0, (img.shape[1], img.shape[0]))
-    elif key == ord('t'):
-        print("Video Rec Stop")
-        record = False
-        video.release()
-
-    if record == True:
-        print("On Rec..")
-        video.write(img)
         
 cap.release()
 cv2.destroyAllWindows()
