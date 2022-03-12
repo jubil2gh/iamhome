7e45c938fa3c1898b71b00b96f0cb31f2454c3d4
jubil2gh <assodium@naver.com>
2022-03-12 23:24:46 +0900

    22.0312.2324

--- kgh/tutorials/ex_mp_face_detection.py
+++ kgh/tutorials/ex_mp_face_detection.py
@@ -5,11 +5,13 @@ Created on Tue Mar  1 19:18:56 2022
 @author: 김관형
 """
 
-
+import numpy as np
 import cv2
 import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
+
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 # For static images:
 IMAGE_FILES = []
@@ -33,6 +35,8 @@ with mp_face_detection.FaceDetection(
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 with mp_face_detection.FaceDetection(
     model_selection=0, min_detection_confidence=0.5) as face_detection:
   while cap.isOpened():
@@ -51,11 +55,21 @@ with mp_face_detection.FaceDetection(
     # Draw the face detection annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    print('Num of OBJ : ', len(results.detections))
     if results.detections:
       for detection in results.detections:
         mp_drawing.draw_detection(image, detection)
+        xmin = int(np.round(detection.location_data.relative_bounding_box.xmin*640, 2))
+        ymin = int(np.round(detection.location_data.relative_bounding_box.ymin*480, 2))
+        width = np.round(detection.location_data.relative_bounding_box.width*640, 2)
+        height = np.round(detection.location_data.relative_bounding_box.height*480, 2)
+        print('hori : ', xmin, width)
+        print('vert : ', ymin, height)
+        TEXT = '{},{}'.format(xmin, ymin)
+        cv2.putText(image,TEXT,(xmin, ymin),font, 1, (0,255,0),2)
+              
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+    cv2.imshow('MediaPipe Face Detection', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
